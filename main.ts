32b68d1ce7d66fedf186299988933d9cce326200
Jorge Lima <jorge-lima@envolve.dev>
2024-03-07 21:43:56 -0300

    Eu acho que deu certo?

--- main.ts
+++ main.ts
@@ -14,36 +14,53 @@ import { booksModel } from "./booksModel.ts";
 let fakeDB: booksModel[] = []; // Banco de dados vazio para armazenar as operações CRUD, isso implica dizer que o servidor armazena os dados nele mesmo, ou seja, quando o servidor é finalizado, os dados vão junto.
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/read", (req,res) => { // Get é uma parte do protocolo HTTP, assim como POST, DELETE e PUT. Porém, como "(req,res)" são parâmetros de callback, eles devem ser devidamente referenciados nas linhas abaixo.
   res.json({ code: "Success", message: 'Sucesso na leitura do banco de dados', result: fakeDB});
 });
 
-app.post("/create", (req,res) => {
-  const body = req.body;
+app.post("/create", (req: Request,res: Response) => {
+  const body = req.body as booksModel;
   const newBook: booksModel = {
-    title: body.title,
-    author: body.author,
-    summ: body.summ,
-    yrpubl: body.yrpubl,
-    ed: body.ed,
+    ...body,
     _id: fakeDB.length+1
   }
 fakeDB.push(newBook);
 
-  return res.send(body);
+  return res.status(201).json(newBook);
 });
 
 app.put("/update", (req,res) => {
+  const {_id} = req.params;
+  const index = fakeDB.findIndex(book => book._id === parseInt(id));
 
+  if (index === -1) {
+    return res.status(404).send("Livro não adicionado.");
+  }
+
+  const updatedBook: booksModel = {
+      ...fakeDB[index],
+      ...req.body,
+      _id: parseInt(id),
+  };
+
+  fakeDB[index] = updatedBook;
+  return res.status (200).json(updatedBook);
+});
+
+app.delete ("/delete", (req: Request, res: Response) => {
+  const {id} = req.params;
+  const initialLength = fakeDB.length;
+  fakeDB = fakeDB.filter (book => book._id !== parseInt(id));
+
+  if (fakeDB.length === initialLength) {
+    return res.status(404).send("Livro não encontrado.");
+  }
+
+  return res.status (204).send();
 })
 
-app.delete ("/delete", (req,res) => {
-
-})
-
-app.listen(8000, function callback(){ //Normalmente se declara a função de callback para verificar o que está havendo no servidor (escutar). Ele é uma função anônima (sem nome), que pode armazenar vários dados e serem manipulados.
-  console.log('Servidor online');
+app.listen(8000, () => { //Normalmente se declara a função de callback para verificar o que está havendo no servidor (escutar). Ele é uma função anônima (sem nome), que pode armazenar vários dados e serem manipulados.
+  console.log('Servidor online na porta {aquela lá}.');
 });
