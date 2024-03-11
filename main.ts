98a8c35236783a9f8606c261d78e81c2e4be410d
Jorge Lima <jorge-lima@envolve.dev>
2024-03-11 17:40:46 -0300

    Reajustando e ajustando o código somente para Deno

--- main.ts
+++ main.ts
@@ -1,66 +1,94 @@
-// O Primeiro passo é iniciar o servidor
-// O segundo é importar os módulos
-// Antes do terceiro, criar as operações CRUD em um fake-DB
-// O terceiro é configurar as variáveis ENV
-// A seguir, configurar a aplicação e rotas
-// Depois, criar um router
-// Então, passar o router como um middleware para a aplicação
-// Por sétimo, configurar o logger e o timer
-// Por fim, insira o comando "denom run --allow-env --allow-net main.ts"
-
-import express from "npm:express";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts"; //Foi retirado o uso do npm para evitar erros entre o Node e Deno
 import { booksModel } from "./booksModel.ts";
 
 let fakeDB: booksModel[] = []; // Banco de dados vazio para armazenar as operações CRUD, isso implica dizer que o servidor armazena os dados nele mesmo, ou seja, quando o servidor é finalizado, os dados vão junto.
 
-const app = express();
-app.use(express.json());
+const router = new Router();
 
-app.get("/read", (req,res) => { // Get é uma parte do protocolo HTTP, assim como POST, DELETE e PUT. Porém, como "(req,res)" são parâmetros de callback, eles devem ser devidamente referenciados nas linhas abaixo.
-  res.json({ code: "Success", message: 'Sucesso na leitura do banco de dados', result: fakeDB});
-});
+router
+.get("/read", (context) => {
 
-app.post("/create", (req: Request,res: Response) => {
-  const body = req.body as booksModel;
-  const newBook: booksModel = {
-    ...body,
-    _id: fakeDB.length+1
-  }
-fakeDB.push(newBook);
-
-  return res.status(201).json(newBook);
-});
-
-app.put("/update", (req,res) => {
-  const {_id} = req.params;
-  const index = fakeDB.findIndex(book => book._id === parseInt(id));
-
-  if (index === -1) {
-    return res.status(404).send("Livro não adicionado.");
-  }
-
-  const updatedBook: booksModel = {
-      ...fakeDB[index],
-      ...req.body,
-      _id: parseInt(id),
-  };
-
-  fakeDB[index] = updatedBook;
-  return res.status (200).json(updatedBook);
-});
-
-app.delete ("/delete", (req: Request, res: Response) => {
-  const {id} = req.params;
-  const initialLength = fakeDB.length;
-  fakeDB = fakeDB.filter (book => book._id !== parseInt(id));
-
-  if (fakeDB.length === initialLength) {
-    return res.status(404).send("Livro não encontrado.");
-  }
-
-  return res.status (204).send();
 })
+.get("/read/:id", (context) => {
+
+})
+.post("/create", async (context) => {
+
+})
+.put("/update/:id", async (context) => {
+
+})
+.delete("/delete/:id", (context) => {
+
+})
+
+const app = new Application();
+
+app.use(async (ctx ,next) => { // Logando o middleware
+
+})
+
+app.use(async (ctx ,next) => { // Temporizando o middleware
+  
+})
+
+app.use(router.routes());
+app.use(router.allowMethods());
+
+await app.listen({ port: 8000});
+console.log ("O servidor está on-line naquela porta lá.");
+
+//app.get("/read", (req,res) => { // Get é uma parte do protocolo HTTP, assim como POST, DELETE e PUT. Porém, como "(req,res)" são parâmetros de callback, eles devem ser devidamente referenciados nas linhas abaixo.
+//  res.json({ code: "Success", message: 'Sucesso na leitura do banco de dados', result: fakeDB});
+//});
+
+//app.post("/create", (req: Request,res: Response) => {
+//  const body = req.body as booksModel;
+//  const newBook: booksModel = {
+//    ...body,
+//    _id: fakeDB.length+1
+//  }
+//fakeDB.push(newBook);
+
+//  return res.status(201).json(newBook);
+//});
+
+//app.put("/update", (req,res) => {
+//  const {_id} = req.params;
+//  const index = fakeDB.findIndex(book => book._id === parseInt(id));
+
+//  if (index === -1) {
+//    return res.status(404).send("Livro não adicionado.");
+//  }
+
+ // const updatedBook: booksModel = {
+  //    ...fakeDB[index],
+   //   ...req.body,
+   //   _id: parseInt(id),
+ // };
+
+ // fakeDB[index] = updatedBook;
+ // return res.status (200).json(updatedBook);
+//});
+
+//app.delete ("/delete", (req: Request, res: Response) => {
+//  const {id} = req.params;
+ // const initialLength = fakeDB.length;
+ // fakeDB = fakeDB.filter (book => book._id !== parseInt(id));
+
+  //if (fakeDB.length === initialLength) {
+    //return res.status(404).send("Livro não encontrado.");
+  //}
+
+  //return res.status (204).send();
+//})
 
 app.listen(8000, () => { //Normalmente se declara a função de callback para verificar o que está havendo no servidor (escutar). Ele é uma função anônima (sem nome), que pode armazenar vários dados e serem manipulados.
   console.log('Servidor online na porta {aquela lá}.');
 });
+
+// Tá faltando a rota de busca via id
+// Falta uma rota de update via id
+// Falta um delete geral (delete-all)
+// Falta uma conexão entre o código e o banco de dados MongoDB
+// Pesquisar sobre o ".gitignore"
